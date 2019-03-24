3817ec55743cad35540301228cd0165bbc0ed179
Alex <blohinalexej@mail.ru>
2019-03-25 01:40:50 +0300

    update p5, var names and refactor to es6 with some bug fix

--- generation.js
+++ generation.js
@@ -1,121 +1,123 @@
-function Generation() {
-	this.arrCeil = [];
-	var m = {}; //координаты мыши
+class Generation {
 
+	constructor() {
+		this.field = []
+	}
 
-	function copyArr(arr) {				
-		var arrCeilBuff = [];
-		for (var i = 0; i < width/k; i++) {
-			arrCeilBuff[i] = [];
-			for (var j = 0; j < height/k; j++){
-				arrCeilBuff[i][j] = arr[i][j];
+	copyArr(arr) {				
+		let new_arr = []
+		for (let i = 0; i < width/k; i++) {
+			new_arr[i] = []
+			for (let j = 0; j < height/k; j++){
+				new_arr[i][j] = arr[i][j]
 			}
 		}
 
-		//расширяем массив, чтобы создать тор
-		arrCeilBuff[-1] = [];
-		arrCeilBuff[width/k] = [];
+		new_arr[-1] = []
+		new_arr[width/k] = []
 
-		arrCeilBuff[-1][-1] = arr[width/k-1][height/k-1];		//левый верхний угол
-		arrCeilBuff[-1][height/k] = arr[width/k-1][0];			//левый нижний угол
-		arrCeilBuff[width/k][-1] = arr[0][height/k-1]; 			//правый верхний угол
-		arrCeilBuff[width/k][height/k] = arr[0][0];				//правый нижний угол
+		new_arr[-1][-1] = arr[width/k - 1][height/k - 1]		//левый верхний угол
+		new_arr[-1][height/k] = arr[width/k - 1][0]					//левый нижний угол
+		new_arr[width/k][-1] = arr[0][height/k - 1] 					//правый верхний угол
+		new_arr[width/k][height/k] = arr[0][0]						//правый нижний угол
 
-		for (var i = 0; i < width/k; i++) {
-			arrCeilBuff[i][-1] = arr[i][height/k-1];
-			arrCeilBuff[i][height/k] = arr[i][0];
+		for (let i = 0; i < width/k; i++) {
+			new_arr[i][-1] = arr[i][height/k - 1]
+			new_arr[i][height/k] = arr[i][0]
 		}
 
-		for (var j = 0; j < height/k; j++) {
-			arrCeilBuff[-1][j] = arr[width/k-1][j];
-			arrCeilBuff[width/k][j] = arr[0][j];
+		for (let j = 0; j < height/k; j++) {
+			new_arr[-1][j] = arr[width/k - 1][j]
+			new_arr[width/k][j] = arr[0][j]
 		}
 
-		return arrCeilBuff;		
+		return new_arr		
 	}
 
 
 
-	this.allIsDead = function() {
-		for (var i = 0; i < width/k; i++) {
-			this.arrCeil[i] = [];
-			for (var j = 0; j < height/k; j++){
-				this.arrCeil[i][j] = 0;
-				rect(i*k, j*k, k, k);
+	startCondition() {
+		for (let i = 0; i < width/k; i++) {
+			this.field[i] = []
+			for (let j = 0; j < height/k; j++){
+				this.field[i][j] = 0
+				rect(i*k, j*k, k, k)
 			}
 		}
 	}
 
 
-	this.new = function() {
-		var arrCeilBuff = copyArr(this.arrCeil);
-		for (var i = 0; i < width/k; i++) {
-			for (var j = 0; j < height/k; j++){
+	new() {
+		let field_buffer = this.copyArr(this.field)
+		for (let i = 0; i < width/k; i++) {
+			for (let j = 0; j < height/k; j++){
 				
 				//считаем живых/мертвых соседей
-				var S = (
-				arrCeilBuff[i-1][j-1]
-				+ arrCeilBuff[i-1][j]
-				+ arrCeilBuff[i-1][j+1]
-				+ arrCeilBuff[i][j-1]					
-				+ arrCeilBuff[i][j+1]
-				+ arrCeilBuff[i+1][j-1]
-				+ arrCeilBuff[i+1][j]
-				+ arrCeilBuff[i+1][j+1]
+				let S = (
+				field_buffer[i-1][j-1]
+				+ field_buffer[i-1][j]
+				+ field_buffer[i-1][j+1]
+				+ field_buffer[i][j-1]					
+				+ field_buffer[i][j+1]
+				+ field_buffer[i+1][j-1]
+				+ field_buffer[i+1][j]
+				+ field_buffer[i+1][j+1]
 				)	
 				
 				// меняем значения по правилам игры
-				if (arrCeilBuff[i][j] == 1 && S < 2 || arrCeilBuff[i][j] == 1 && S > 3 ) {
-					this.arrCeil[i][j] = 0;
+				if (field_buffer[i][j] == 1 && (S < 2 || S > 3)) {
+					this.field[i][j] = 0
 				}	
-				if (arrCeilBuff[i][j] == 0 && S == 3) {
-					this.arrCeil[i][j] = 1;
+				if (field_buffer[i][j] == 0 && S == 3) {
+					this.field[i][j] = 1
 				}
 			}
 		}
 	}
 
-	this.draw = function() {
-		clear(0, 0, width, height);
-		for (var i = 0; i < width/k; i++) {
-			for (var j = 0; j < height/k; j++){
-				if (this.arrCeil[i][j] == 1) {
-					rect(i*k, j*k, k, k);
-					var rx = (i*k+(i+1)*k)/2;
-					var ry = (j*k+(j+1)*k)/2;
-					fill("black");
-					ellipse(rx, ry, k-4);
-					noFill();
+	draw() {
+		for (let i = 0; i < width/k; i++) {
+			for (let j = 0; j < height/k; j++){
+				if (this.field[i][j] == 1) {
+					rect(i*k, j*k, k, k)
+					const rx = (i*k + (i+1)*k) / 2
+					const ry = (j*k + (j+1)*k) / 2
+					fill('black')
+					ellipse(rx, ry, k - 4)
+					noFill()
 				}
-				if (this.arrCeil[i][j] == 0)	{
-					rect(i*k, j*k, k, k);
+				if (this.field[i][j] == 0)	{
+					rect(i*k, j*k, k, k)
 				}
 			}
 		}		
 	}
 
-	this.getAndDraw = function(arg) {	
-		if (mouseX < width) m.x = mouseX;
-		if (mouseY < height) m.y = mouseY;
-		while (m.x%k != 0) m.x--;
-		while (m.y%k != 0) m.y--;
-		var i = m.x/k;
-		var j = m.y/k;
-		switch (this.arrCeil[i][j]) {
+	getAndDraw(arg) {
+		let m = {}
+		if (mouseX < width) m.x = mouseX
+		if (mouseY < height) m.y = mouseY
+		while (m.x % k != 0) m.x--
+		while (m.y % k != 0) m.y--
+		
+		let i = m.x/k
+		let j = m.y/k
+
+		switch (this.field[i][j]) {
 			case 1:
-				if (arg == "can kill") { 
-					this.arrCeil[i][j] = 0;
-					fill("white");
-					ellipse( (i*k+(i+1)*k)/2, (j*k+(j+1)*k)/2, k-4);
-					noFill();
+				if (arg == 'can kill') { 
+					this.field[i][j] = 0
+					fill('white')
+					ellipse( (i*k + (i+1)*k) / 2, (j*k + (j+1)*k) / 2, k - 4)
+					noFill()
 				}
-				break;
+				break
 			case 0:
-				this.arrCeil[i][j] = 1;
-				fill("black");
-				ellipse( (i*k+(i+1)*k)/2, (j*k+(j+1)*k)/2, k-4);
-				noFill();
-				break;
+				this.field[i][j] = 1
+				fill('black')
+				ellipse( (i*k + (i+1)*k) / 2, (j*k + (j+1)*k) / 2, k - 4)
+				noFill()
+				break
 
 		}
 	}	
--- sketch.js
+++ sketch.js
@@ -1,43 +1,45 @@
-var k = 20; //сторона* клетки 
-var gen;
-var p = 0; // счетчик остановок
-var pastX, pastY;
+const k = 20 // ceil size
+let p = 0 // space bar counter
+let gen, last_x, last_y
 
 
 function setup() {
-	createCanvas(401, 401);
-	width -= 1;
-	height -= 1;
-	rect(0, 0, width, height);
-	gen = new Generation();
-	gen.allIsDead();
-	frameRate(12);
+	createCanvas(801, 801)
+	width -= 1
+	height -= 1
+	gen = new Generation()
+	gen.startCondition()
+	frameRate(10)
 }
 
 function draw() {
-	gen.new();
-	gen.draw();	
-
+	clear()
+	gen.new()
+	gen.draw()	
 }
 
 function keyPressed() {
-	if (keyCode == 32 && p%2 == 0) {
-		noLoop();
-		p++;
-	} else if (keyCode == 32 && p%2 != 0) {
-		loop();
-		p++;
+	if (keyCode == 32) {
+		switch (p % 2) {
+			case 0:
+				noLoop()
+				break
+			case 1:
+				loop()
+				break
+		}
+		p++
 	}
 }
 
 function mousePressed() {
-	gen.getAndDraw('can kill');
-	pastX = mouseX;
-	pastY = mouseY;
+	gen.getAndDraw('can kill')
+	last_x = mouseX
+	last_y = mouseY
 }
 
 function mouseDragged() {
-	if (dist(pastX, pastY, mouseX, mouseY) > 5 ) {
-		gen.getAndDraw();
+	if (dist(last_x, last_y, mouseX, mouseY) > 5 ) {
+		gen.getAndDraw()
 	}
 }
